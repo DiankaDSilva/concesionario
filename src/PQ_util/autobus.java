712d045d03df4c02aa403317651931a93042ba4c
ipicallos <inakipicallos@gmail.com>
2024-01-25 12:28:56 +0100

    clase autobus iñaki

---
+++ src/PQ_util/autobus.java
@@ -0,0 +1,17 @@
+package PQ_util;
+
+public class autobus  {
+	public String marca;
+	public String modelo;
+	public int cilindrada;
+	public String combustible;
+	public String matricula;
+	public String color;
+	public int numpuertas;
+	public int plazas;
+	
+	
+	public void imprimir() {
+		System.out.println("marca: "+marca+" modelo: "+modelo+" matricula: "+matricula);
+	}
+}
